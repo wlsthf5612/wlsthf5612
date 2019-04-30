77c31db7cf67c0b11d33640c54bc625cafdb5085
wlsthf5612 <wlsthf5612@naver.com>
2019-04-30 11:24:38 +0900

    스위치 기능을 추가하여 스위치1은 300ms로 스위치2은 100ms로 동작하도록 기능 추가함

--- Src/main.c
+++ Src/main.c
@@ -99,10 +99,22 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+  int dealyValue = 100;
   while (1)
   {
+	 if( HAL_GPIO_ReadPin(GPIOB, GPIO_PIN0) == 0)
+	 {
+		 delayValue = 300;
+		// HAL_Delay(300);
+	 }
+
+	 else if( HAL_GPIO_ReadPin(GPIOB, GPIO_PIN1) == 0 );
+	 {
+		 delayValue = 100;
+		 //HAL_Delay(100);
+	 }
 	  HAL_GPIO_TogglePin(GPIOC,GPIO_PIN_13);
-	  HAL_Delay(1000);
+	  HAL_Delay(delayValue);
 
   /* USER CODE END WHILE */
 
@@ -178,6 +190,7 @@ static void MX_GPIO_Init(void)
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
@@ -189,6 +202,12 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PB0 PB1 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
 }
 
